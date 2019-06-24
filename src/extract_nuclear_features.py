cef4e8ac6e0f0ee6a1d9e17da2153edaef282df5
Gabriele Girelli <gigi.ga90@gmail.com>
2019-06-24 13:25:43 +0200

    Added parallelization

--- src/extract_nuclear_features.py
+++ src/extract_nuclear_features.py
@@ -20,6 +20,8 @@ import pygpseq as gp
 import re
 import sys
 
+from ggc.args import check_threads
+from joblib import Parallel, delayed
 from skimage.io import imread
 from tqdm import tqdm
 
@@ -44,6 +46,8 @@ parser.add_argument('-p', '--prefix', type = str, help = '''
 parser.add_argument('-a', '--aspect', type = float, nargs = 3,
 	help = """Physical size of Z, Y and X voxel sides in nm. Default: 300.0 216.6 216.6""",
 	metavar = ('Z', 'Y', 'X'), default = [300., 216.6, 216.6])
+parser.add_argument('-t', '--threads', metavar = 'nthreads', type = int,
+	default = 1, help = """Number of threads to be used for parallelization.""")
 
 # Version flag
 version = "0.0.1"
@@ -59,7 +63,40 @@ assert os.path.isdir(args.outdir)
 if type(None) == type(args.prefix):
 	args.prefix = os.basename(args.outdir)
 
+args.threads = check_threads(args.threads)
+
 # FUNCTIONS ====================================================================
+
+def get_nucleus_feats(signature, condition):
+	sid, nid = [int(x) for x in re.match(r's([0-9]+)n([0-9]+)',"s12n23").groups()]
+
+	mask = gp.tools.image.add_top_bottom_slides(
+		imread(os.path.join(args.rootdir, f'{signature}{condition}.tif')))
+	idna = gp.tools.image.add_top_bottom_slides(
+		imread(os.path.join(args.rootdir, f'{signature}{condition}.dna.tif')))
+	isig = gp.tools.image.add_top_bottom_slides(
+		imread(os.path.join(args.rootdir, f'{signature}{condition}.sig.tif')))
+
+	nRadius = (mask.sum()*vxVolume*3/4/np.pi)**(1/3)
+	nSurface = gp.tools.image.calc_surface(mask, args.aspect)
+
+	nData = {
+		'condition' : [condition],						# Dataset
+		'sid' : [sid], 'nid' : [nid],					# Series/Nucleus ID
+		'area_px' : mask.max(0).sum(),					# Z-projection area in px
+		'area_um2' : mask.max(0).sum()*pxArea,			# Z-projection area in um2
+		'volume_vx' : [mask.sum()],						# Volume in vx
+		'volume_um3' : [mask.sum()*vxVolume],			# Volume in um3
+		'radius_um' : [nRadius],						# Radius in um
+		'dna_sum' : [np.nansum(idna[mask != 0])],		# DNA intensity integral
+		'dna_mean' : [np.nanmean(idna[mask != 0])],		# DNA intensity mean
+		'sig_sum' : [np.nansum(isig[mask != 0])],		# Signal intensity integral
+		'sig_mean' : [np.nanmean(isig[mask != 0])],		# Signal intensity mean
+		'surface_um2' : [nSurface],						# Surface in um2
+		'sphericity' : 4*np.pi*(nRadius**2)/nSurface	# Sphericity
+	}
+
+	return(pd.DataFrame.from_dict(nData))
 
 # RUN ==========================================================================
 
@@ -80,40 +117,10 @@ vxVolume = np.prod(args.aspect)*1e-9 # (um3)
 
 data = []
 for condition in tqdm(list(nuclei_dict.keys()), desc = "Condition"):
-	cData = []
-	for signature in tqdm(nuclei_dict[condition], desc = "Nuclei"):
-		sid, nid = [int(x) for x in re.match(r's([0-9]+)n([0-9]+)',"s12n23").groups()]
-
-		mask = gp.tools.image.add_top_bottom_slides(
-			imread(os.path.join(args.rootdir, f'{signature}{condition}.tif')))
-		idna = gp.tools.image.add_top_bottom_slides(
-			imread(os.path.join(args.rootdir, f'{signature}{condition}.dna.tif')))
-		isig = gp.tools.image.add_top_bottom_slides(
-			imread(os.path.join(args.rootdir, f'{signature}{condition}.sig.tif')))
-
-		nRadius = (mask.sum()*vxVolume*3/4/np.pi)**(1/3)
-		nSurface = gp.tools.image.calc_surface(mask, args.aspect)
-
-		nData = {
-			'condition' : [condition],						# Dataset
-			'sid' : [sid], 'nid' : [nid],					# Series/Nucleus ID
-			'area_px' : mask.max(0).sum(),					# Z-projection area in px
-			'area_um2' : mask.max(0).sum()*pxArea,			# Z-projection area in um2
-			'volume_vx' : [mask.sum()],						# Volume in vx
-			'volume_um3' : [mask.sum()*vxVolume],			# Volume in um3
-			'radius_um' : [nRadius],						# Radius in um
-			'dna_sum' : [np.nansum(idna[mask != 0])],		# DNA intensity integral
-			'dna_mean' : [np.nanmean(idna[mask != 0])],		# DNA intensity mean
-			'sig_sum' : [np.nansum(isig[mask != 0])],		# Signal intensity integral
-			'sig_mean' : [np.nanmean(isig[mask != 0])],		# Signal intensity mean
-			'surface_um2' : [nSurface],						# Surface in um2
-			'sphericity' : 4*np.pi*(nRadius**2)/nSurface	# Sphericity
-		}
-
-		cData.append(pd.DataFrame.from_dict(nData))
-
+	cData = Parallel(n_jobs = args.threads, verbose = 11)(
+	    delayed(get_nucleus_feats)(signature, condition)
+	    for signature in nuclei_dict[condition])
 	data.append(pd.concat(cData))
-
 outData = pd.concat(data).reset_index(drop = True)
 
 outData.to_csv(os.path.join(args.outdir, f"{args.prefix}.nuclear_features.tsv"),
