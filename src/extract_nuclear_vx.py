cbc1575359c95ab47e7ca781cf976f0e63224eeb
Gabriele Girelli <gigi.ga90@gmail.com>
2019-06-25 16:51:33 +0200

    Fixed center definition and added midsection 2D analysis option

--- src/extract_nuclear_vx.py
+++ src/extract_nuclear_vx.py
@@ -27,14 +27,12 @@ from tqdm import tqdm
 
 # PARAMETERS ===================================================================
 
-# Add script description
 parser = argparse.ArgumentParser(description = '''
 Extract single nucleus voxel data from nuclear boxes, generating using the -u flag
 of the gpseq_anim script. Use also --nuclear-sel with no parameters to extract
 all nuclear boxes (useful if new selection should be performed).
 ''', formatter_class = argparse.RawDescriptionHelpFormatter)
 
-# Add mandatory arguments
 parser.add_argument('prefix', type = str, help = '''
 	User prefix (usually in the format "iFL").''')
 parser.add_argument('rootdir', type = str, help = '''
@@ -42,25 +40,41 @@ parser.add_argument('rootdir', type = str, help = '''
 parser.add_argument('outdir', type = str, help = '''
 	Path to output directory where the output should be written to.''')
 
-# Add arguments with default value
 parser.add_argument('-a', '--aspect', type = float, nargs = 3,
 	help = """Physical size of Z, Y and X voxel sides in nm. Default: 300.0 216.6 216.6""",
 	metavar = ('Z', 'Y', 'X'), default = [300., 216.6, 216.6])
 parser.add_argument('-t', '--threads', metavar = 'nthreads', type = int,
 	default = 1, help = """Number of threads to be used for parallelization.""")
+parser.add_argument('--dist-type', type = str,
+	help = """Method for lamina distance calculation/normalization.
+	Default: '%s'""" % (gp.const.LD_ARG_LABELS[gp.const.LD_DEFAULT]),
+	choices = list(gp.const.LD_ARG_LABELS),
+	default = gp.const.LD_ARG_LABELS[gp.const.LD_DEFAULT])
+parser.add_argument('--mid-type', type = str,
+	help = """Method for mid-section selection. Default: '%s'""" % (
+		gp.const.MID_SEC_ARG_LABELS[gp.const.MID_SEC_DEFAULT]),
+	choices = list(gp.const.MID_SEC_ARG_LABELS),
+	default = gp.const.MID_SEC_ARG_LABELS[gp.const.MID_SEC_DEFAULT])
 
-# Version flag
+parser.add_argument('-2', action = 'store_const', dest = 'only_from_mid',
+	const = True, default = False, help = '''Extract only voxels from the nucleus mid section.
+	Check the --mid-type option for how to change section selection method.''')
+
 version = "0.0.1"
 parser.add_argument('--version', action = 'version',
 	version = '%s v%s' % (sys.argv[0], version,))
 
-# Parse arguments
 args = parser.parse_args()
+
+assert os.path.isdir(args.rootdir)
+assert os.path.isdir(args.outdir)
 
 if type(None) == type(args.prefix):
 	args.prefix = os.basename(args.outdir)
 
 args.threads = check_threads(args.threads)
+args.mid_type = gp.const.MID_SEC_ARG_LABELS.index(args.mid_type)
+args.dist_type = gp.const.LD_ARG_LABELS.index(args.dist_type)
 
 # FUNCTIONS ====================================================================
 
@@ -71,15 +85,16 @@ def buildVxNuclearProfile(n, condition):
 	idna = gp.tools.image.add_top_bottom_slides(imread(f'{args.rootdir}/{sample}.dna.tif'))
 	isig = gp.tools.image.add_top_bottom_slides(imread(f'{args.rootdir}/{sample}.sig.tif'))
 
+	if args.only_from_mid:
+		largestID = gp.tools.image.get_mid_section_idx(idna, mask, args.mid_type)
+		mask = mask[largestID]
+		idna = idna[largestID]
+		isig = isig[largestID]
+
 	ncom = np.array(center_of_mass(mask)).astype("i")
 
-	empty = np.zeros(mask.shape)
-	empty[ncom[0], ncom[1], ncom[2]] = 1
-	empty -= 1
-	empty *= -1
-
-	laminD = gp.tools.distance.calc_lamina_distance(mask, args.aspect)
-	centrD = gp.tools.distance.calc_lamina_distance(empty, args.aspect)
+	laminD, centrD = gp.tools.distance.calc_nuclear_distances(
+		args.dist_type, mask, args.aspect)
 
 	mask_flat = mask.reshape([np.prod(mask.shape)])
 	mask_flat = np.where(mask_flat.tolist())[0].tolist()
