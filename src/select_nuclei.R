0ab694bb293c5555d431237e9941d3733cca3d9c
Gabriele Girelli <gigi.ga90@gmail.com>
2019-07-04 18:18:08 +0200

    Removed unnecessary comments.

--- src/select_nuclei.R
+++ src/select_nuclei.R
@@ -44,17 +44,13 @@ parser = add_argument(parser, arg = '--date-meta', short = "-d", type = class(""
 version_flag = "0.0.1"
 parser = add_argument(parser, arg = '--version', short = '-V',
 	help = 'Print version and quit.', flag = T)
-
 args = commandArgs(trailingOnly=TRUE)
 if ( "--version" %in% args ) {
 	cat(sprintf("%s v%s\n", scriptName, version_flag))
 	quit()
 }
 
-# Parse arguments
 p = parse_args(parser)
-
-# Attach argument values to variables
 attach(p['' != names(p)], warn.conflicts = F)
 
 if ( !file.exists(inpath) )
